722370b472b8405e07569823d30053435ca9edae
mishra38 <Anu.Mishra2@emc.com>
2021-06-13 11:23:10 +0530

    Test Cases added

--- src/test/java/covid/dashboard/UserRepositoryTests.java
+++ src/test/java/covid/dashboard/UserRepositoryTests.java
@@ -11,7 +11,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
@@ -26,9 +25,6 @@ public class UserRepositoryTests {
 	
 	@Autowired
 	private UserRepository repository;
-
-	@MockBean
-	TestRestTemplate restTemplate;
 	
 	@Test
 	public void testCreateUser() {
